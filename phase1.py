fab7455d583987b32af7f7de0dcb5bb5c4a26149
Ali <alijfri99@gmail.com>
2021-04-21 02:26:06 +0430

    The first question is almost done.

--- phase1.py
+++ phase1.py
@@ -1,22 +1,38 @@
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 dataset = pd.read_csv("divar_posts_dataset.csv")
-dataset.boxplot()
+
+# correcting a value in the year column, and then converting the year column to numeric
+dataset['year'].loc[(dataset['year'] == '<1366')] = '1366'
+dataset['year'] = pd.to_numeric(dataset['year'])
+
+print(dataset.columns)
 
 # printing the name of each column and the unique values of each column
 for column in dataset.columns:
     if dataset.columns.get_loc(column) == 0:
         continue
-    print(column)
+
+    print("Column name:", column)
     print(dataset[column].unique())
-    is_nominal = input("Is this data numeric?")
-    if is_nominal == 'y':
+
+    request = input("Would you like to see every unique value for this column?")
+    if request == 'y':
+        for unique_value in dataset[column].unique():
+            print(unique_value)
+
+    is_numeric = input("Is this data numeric?")
+    if is_numeric == 'y':
         print("Min:", dataset[column].min())
         print("Max:", dataset[column].max())
         print("Mean:", dataset[column].mean())
         print("Mode:", dataset[column].mode())
         print("Median:", dataset[column].median())
+        plt.figure()
+        boxplot = sns.boxplot(data=dataset[column])
+        plt.show()
     else:
         print("Mode:", dataset[column].mode())
         print("*****\n*****\n*****")
