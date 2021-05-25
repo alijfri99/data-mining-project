de4fbcead87c047f98ce2477fc3670598c084756
Ali <alijfri99@gmail.com>
2021-05-26 02:02:36 +0430

    Started working on phase 2

---
+++ phase2-1.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from apyori import apriori
+
+# question 1
+
+dataset = pd.read_csv("divar_posts_dataset.csv")
+
+print(dataset['city'].isna().sum())
+print(dataset['cat1'].isna().sum())
+print(dataset['cat2'].isna().sum())
+print(dataset['cat3'].isna().sum())
+
+dataset_q1_1 = dataset[['city', 'cat1']]
+print(dataset_q1_1.head())
+print(dataset_q1_1.isna().sum())
+dataset_q1_1 = dataset_q1_1.values.tolist()
+rules_1_1 = apriori(dataset_q1_1, min_support=0.01, min_confidence=0.2)
+
+for i in list(rules_1_1):
+    print(i)
+
+print("\n***********************************************************************************")
+print("***********************************************************************************")
+print("***********************************************************************************\n")
+
+dataset_q1_2 = dataset[['city', 'cat2']]
+dataset_q1_2 = dataset_q1_2.dropna(subset=['cat2'])
+print(dataset_q1_2.head())
+print(dataset_q1_2.isna().sum())
+dataset_q1_2 = dataset_q1_2.values.tolist()
+rules_1_2 = apriori(dataset_q1_2, min_support=0.01, min_confidence=0.2)
+
+for i in list(rules_1_2):
+    print(i)
+
+print("\n***********************************************************************************")
+print("***********************************************************************************")
+print("***********************************************************************************\n")
+
+dataset_q1_3 = dataset[['city', 'cat3']]
+dataset_q1_3 = dataset_q1_3.dropna(subset=['cat3'])
+print(dataset_q1_3.head())
+print(dataset_q1_3.isna().sum())
+dataset_q1_3 = dataset_q1_3.values.tolist()
+rules_1_3 = apriori(dataset_q1_3, min_support=0.01, min_confidence=0.2)
+
+for i in list(rules_1_3):
+    print(i)
