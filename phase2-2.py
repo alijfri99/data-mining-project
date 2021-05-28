58a19c7599b7f54edc56b7dbc618e5985ec3dc55
Ali <alijfri99@gmail.com>
2021-05-28 14:22:21 +0430

    The second phase is finished.

--- phase2-2.py
+++ phase2-2.py
@@ -6,12 +6,12 @@ from apyori import apriori
 dataset = pd.read_csv("orders.csv")
 
 # preprocessing
-
-dataset = dataset.dropna()
+print(dataset['ID_Order'].is_unique)
+print(dataset.isna().sum())
+dataset = dataset.drop(['ID_Customer', 'Quantity_item'], axis=1)
 dataset['DateTime_CartFinalize'] = dataset['DateTime_CartFinalize'].str[0:10]
 dataset['ID_Order'] = 'Ord' + dataset['ID_Order'].map(str)
 dataset['ID_Item'] = 'It' + dataset['ID_Item'].map(str)
-dataset = dataset.drop(['ID_Customer', 'Quantity_item'], axis=1)
 
 # now let's construct the transaction dictionary for each Order ID
 
@@ -25,6 +25,44 @@ for index, row in dataset.iterrows():
     else:
         transaction_dict[row['ID_Order']] = transaction_dict[row['ID_Order']].union({row['ID_Item']})
 
+# now let's convert the dictionary to a list of lists
+
+transactions = list()
+
 for i in transaction_dict:
-    if len(transaction_dict[i]) > 1:
-        print(i, transaction_dict[i])
+    transactions.append(list(transaction_dict[i]))
+
+rules_2_1 = apriori(transactions, min_support=0.0001, min_confidence=0.0001, min_lift=1.000001)
+
+rules_2_1 = list(rules_2_1)
+
+for i in rules_2_1:
+    print(str(rules_2_1.index(i)) + ".", i)
+
+print("\n***********************************************************************************")
+print("***********************************************************************************")
+print("***********************************************************************************\n")
+
+dataset_q2_2 = dataset[['city_name_fa', 'ID_Item']]
+print(dataset_q2_2.head())
+print(dataset_q2_2.isna().sum())
+dataset_q2_2 = dataset_q2_2.values.tolist()
+rules_2_2 = apriori(dataset_q2_2, min_support=0.0001, min_confidence=0.2, min_lift=1.000001)
+
+rules_2_2 = list(rules_2_2)
+for i in rules_2_2:
+    print(str(rules_2_2.index(i)) + ".", i)
+
+print("\n***********************************************************************************")
+print("***********************************************************************************")
+print("***********************************************************************************\n")
+
+dataset_q2_3 = dataset[['DateTime_CartFinalize', 'ID_Item']]
+print(dataset_q2_3.head())
+print(dataset_q2_3.isna().sum())
+dataset_q2_3 = dataset_q2_3.values.tolist()
+rules_2_3 = apriori(dataset_q2_3, min_support=0.0001, min_confidence=0.1, min_lift=1.000001)
+
+rules_2_3 = list(rules_2_3)
+for i in rules_2_3:
+    print(str(rules_2_3.index(i)) + ".", i)
